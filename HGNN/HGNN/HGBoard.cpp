9f26407d042e36ced1a51057341bb6204528e714
vivisuke <ntsuda@master.email.ne.jp>
2020-01-31 09:46:38 +0900

    HGBoard.h cpp 追加

---
+++ HGNN/HGNN/HGBoard.cpp
@@ -0,0 +1,486 @@
+#include <iostream>
+#include <assert.h>
+#include <unordered_map>
+#include <unordered_set>
+#include "utils.h"
+#include "HGBoard.h"
+
+using namespace std;
+
+static unordered_set<string> s_set_board;
+
+HGBoard::HGBoard()
+{
+	m_board.resize(HG_BOARD_SIZE);
+	m_black = &m_board[0];
+	m_white = &m_board[HG_ARY_SIZE];
+	init();
+}
+//---------------------------------------------------
+string Move::text() const {
+	string txt = to_string((int)m_src) + "/" + to_string((int)m_src - m_d);
+	if (m_hit) txt += '*';
+	return txt;
+}
+//---------------------------------------------------
+HGBoard::HGBoard(const HGBoard& x)
+{
+	m_board = x.m_board;
+	m_black = &m_board[0];
+	m_white = &m_board[HG_ARY_SIZE];
+	m_nBlack = x.m_nBlack;
+	m_nWhite = x.m_nWhite;
+}
+HGBoard::HGBoard(cchar* black, cchar* white)
+{
+	m_board.resize(HG_BOARD_SIZE);
+	m_black = &m_board[0];
+	m_white = &m_board[HG_ARY_SIZE];
+	m_nBlack = m_nWhite = 0;
+	for (int i = HG_GOAL_IX; i != HG_ARY_SIZE; ++i) {
+		m_nBlack += (m_black[i] = black[i]);
+		m_nWhite += (m_white[i] = white[i]);
+	}
+}
+bool HGBoard::operator==(const HGBoard& x) const {
+	return m_board == x.m_board;
+}
+void HGBoard::init()
+{
+	clear();
+	m_black[3] = m_black[7] = 3;
+	m_black[12] = 2;
+	m_white[3] = m_white[7] = 3;
+	m_white[12] = 2;
+	m_nBlack = m_nWhite = 8;
+}
+void HGBoard::clear()
+{
+	for (auto& x : m_board) x = 0;
+	m_nBlack = m_nWhite = 0;
+}
+void HGBoard::swapBW()
+{
+	for (int i = HG_GOAL_IX; i != HG_ARY_SIZE; ++i)
+		std::swap(m_black[i], m_white[i]);
+	std::swap(m_nBlack, m_nWhite);
+}
+void HGBoard::set(const std::string& ktext)
+{
+	m_board = ktext;
+	for(auto& x: m_board) x -= '0';
+	updateNumBW();
+}
+void	HGBoard::updateNumBW()				//	白黒石数再計算
+{
+	m_nBlack = m_nWhite = 0;
+	for (int i = HG_START_IX; i >= HG_GOAL_IX; --i) {
+		m_nBlack += m_black[i];
+		m_nWhite += m_white[i];
+	}
+}
+void HGBoard::b_setAt(int ix, int n)
+{
+	m_nBlack -= m_black[ix];
+	m_nBlack += (m_black[ix] = n);
+}
+void HGBoard::w_setAt(int ix, int n)
+{
+	m_nWhite -= m_white[ix];
+	m_nWhite += (m_white[ix] = n);
+}
+static int pips(cchar* ptr) {
+	int pips = 0;
+	for (int ix = HG_GOAL_IX; ix <= HG_START_IX; ++ix)
+		pips += ptr[ix] * ix;
+	return pips;
+}
+int HGBoard::b_pips() const {
+	return pips(m_black);
+}
+int HGBoard::w_pips() const {
+	return pips(m_white);
+}
+int HGBoard::result() const {
+	if (m_black[HG_GOAL_IX] == m_nBlack)
+		return 1;
+	else if (m_white[HG_GOAL_IX] == m_nWhite)
+		return -1;
+	else
+		return 0;
+}
+int HGBoard::resultSGB() const		//	シングル・ギャモン・バックギャモン勝負を判定、{-3, -2, -1, 0, +1, +2, +3} を返す
+{
+	if (m_black[HG_GOAL_IX] == m_nBlack) {
+		if( m_white[HG_GOAL_IX] == 0 ) {
+			if( m_white[HG_START_IX] != 0 || m_white[HG_START_IX-1] != 0 ||
+				m_white[HG_START_IX-2] != 0 || m_white[HG_START_IX-3] != 0 )
+			{
+				return 3;
+			}
+			if( m_white[HG_START_IX-4] != 0 || m_white[HG_START_IX-5] != 0 || m_white[HG_START_IX-6] != 0 ||
+				m_white[HG_START_IX-7] != 0 || m_white[HG_START_IX-8] != 0 || m_white[HG_START_IX-9] != 0 )
+			{
+				return 2;
+			}
+		}
+		return 1;
+	} else if (m_white[HG_GOAL_IX] == m_nWhite) {
+		if( m_black[HG_GOAL_IX] == 0 ) {
+			if( m_black[HG_START_IX] != 0 || m_black[HG_START_IX-1] != 0 ||
+				m_black[HG_START_IX-2] != 0 || m_black[HG_START_IX-3] != 0 )
+			{
+				return -3;
+			}
+			if( m_black[HG_START_IX-4] != 0 || m_black[HG_START_IX-5] != 0 || m_black[HG_START_IX-6] != 0 ||
+				m_black[HG_START_IX-7] != 0 || m_black[HG_START_IX-8] != 0 || m_black[HG_START_IX-9] != 0 )
+			{
+				return -2;
+			}
+		}
+		return -1;
+	} else
+		return 0;
+}
+std::string HGBoard::ktext() const
+{
+	string txt = m_board;
+	for(auto& x: txt) x += '0';
+	return txt;
+}
+string HGBoard::text() const
+{
+	//  undone: ヒットされた石表示
+	string txt;
+	int bp = b_pips(), wp = w_pips();
+	txt += "pips black: " + to_string(bp) + "(" + to_string(bp - wp) +
+		") white: " + to_string(wp) + "(" + to_string(wp - bp) + ")\n";
+	txt += " ";
+	if (m_black[HG_START_IX] != 0) {
+		txt += "● ";
+		txt += to_string((int)m_black[HG_START_IX]);
+	}
+	else
+		txt += "	";
+	txt += string(30, ' ');
+	//txt += "x ◆";
+	if (m_white[HG_START_IX] != 0) {
+		txt += to_string((int)m_white[HG_START_IX]);
+		txt += " ○";
+	}
+	else
+		txt += "	";
+	txt += "\n";
+	txt += "+--+--------+--------+--------+--------+--+\n";
+	for (int y = 8; y != 0; --y) {
+		txt += "|";
+		for (int x = HG_START_IX; x >= HG_GOAL_IX; --x) {
+			if (x != HG_START_IX && m_black[x] >= y)
+				txt += "●";
+			else if (hg_revIX(x) != HG_START_IX && m_white[hg_revIX(x)] >= y)
+				txt += "○";
+			else
+				txt += "・";
+			if (x == HG_START_IX || x % 3 == 1 || x == HG_GOAL_IX)
+				txt += "|";
+			else
+				txt += " ";
+		}
+		txt += "\n";
+	}
+	txt += "+--+--------+--------+--------+--------+--+\n";
+	txt += " Ｓ 12 11 10 ９ ８ ７ ６ ５ ４ ３ ２ １ Ｇ\n";
+	return txt;
+}
+void HGBoard::b_genMoves(Moves& mvs, int d) const
+{
+	//cout << "b_genMoves(d = " << d << ")\n";
+	int ix = HG_START_IX;
+	while (m_black[ix] == 0) --ix;	 //  黒石は必ず存在すると仮定、なので範囲チェックは行わない
+	if (ix == 0) {
+		//exit(0);
+		//assert(0);
+		return;
+	}
+	//cout << "ix = " << ix << "\n";
+	if (ix == HG_START_IX) {
+		//cout << "bar\n";
+		int wn = m_white[hg_revIX(ix - d)];
+		//cout << "wn = " << wn << "\n";
+		if (wn < 2) {
+			mvs.push_back(Move(ix, d, wn == 1));
+		}
+	}
+	else if (ix <= HG_INNER) {
+		//cout << "inner\n";
+		bool tail = true;
+		for (; ix != 0; --ix, tail = false) {
+			if (m_black[ix] != 0) {
+				int rx;
+				if( ix == d ) {
+					mvs.push_back(Move(ix, d));
+				} else if (ix > d && m_white[rx = hg_revIX(ix-d)] < 2 ) {
+					mvs.push_back(Move(ix, d, m_white[rx] == 1));
+				} else if (tail)
+					mvs.push_back(Move(ix, ix));
+			}
+		}
+	}
+	else {
+		while (ix != 0) {
+			if (m_black[ix] != 0 && ix - d > 0) {
+				int wn = m_white[hg_revIX(ix - d)];
+				if (wn < 2)
+					mvs.push_back(Move(ix, d, wn == 1));
+			}
+			--ix;
+		}
+	}
+}
+void HGBoard::b_genMovesListSeq(MovesList& lst, int d1, int d2) const
+{
+	Moves mvs;
+	b_genMoves(mvs, d1);
+	for (const auto& mv : mvs) {
+		HGBoard b2(*this);
+		b2.b_move(mv);
+		Moves mvs2;
+		b2.b_genMoves(mvs2, d2);
+		if (mvs2.empty()) {
+			lst.push_back(Moves());
+			lst.back().push_back(mv);
+		}
+		else {
+			for (auto mv2 : mvs2) {
+				HGBoard b3(b2);
+				b3.b_move(mv2);
+				if (s_set_board.find(b3.m_board) == s_set_board.end()) {
+					s_set_board.insert(b3.m_board);
+					lst.push_back(Moves());
+					lst.back().push_back(mv);
+					lst.back().push_back(mv2);
+				}
+			}
+		}
+	}
+}
+void HGBoard::b_genMovesList(MovesList& lst, int d1, int d2) const
+{
+	s_set_board.clear();
+	lst.clear();
+	if (d1 != d2) {	//  ゾロ目ではない場合
+		b_genMovesListSeq(lst, d1, d2);
+		b_genMovesListSeq(lst, d2, d1);
+	}
+	else {			//  ゾロ目の場合
+		Moves mvs1;
+		b_genMoves(mvs1, d1);
+		for (const auto& mv1 : mvs1) {
+			HGBoard b2(*this);
+			b2.b_move(mv1);
+			Moves mvs2;
+			b2.b_genMoves(mvs2, d1);
+			if (mvs2.empty()) {
+				if (s_set_board.find(b2.m_board) == s_set_board.end()) {
+					s_set_board.insert(b2.m_board);
+					lst.push_back(Moves());
+					lst.back().push_back(mv1);
+				}
+			}
+			else {
+				for (auto mv2 : mvs2) {
+					HGBoard b3(b2);
+					b3.b_move(mv2);
+					Moves mvs3;
+					b3.b_genMoves(mvs3, d1);
+					if (mvs3.empty()) {
+						if (s_set_board.find(b3.m_board) == s_set_board.end()) {
+							s_set_board.insert(b3.m_board);
+							lst.push_back(Moves());
+							lst.back().push_back(mv1);
+							lst.back().push_back(mv2);
+						}
+					}
+					else {
+						for (auto mv3 : mvs3) {
+							HGBoard b4(b3);
+							b4.b_move(mv3);
+							Moves mvs4;
+							b4.b_genMoves(mvs4, d1);
+							if (mvs4.empty()) {
+								if (s_set_board.find(b4.m_board) == s_set_board.end()) {
+									s_set_board.insert(b4.m_board);
+									lst.push_back(Moves());
+									lst.back().push_back(mv1);
+									lst.back().push_back(mv2);
+									lst.back().push_back(mv3);
+								}
+							}
+							else {
+								for (auto mv4 : mvs4) {
+									HGBoard b5(b4);
+									b5.b_move(mv4);
+									if (s_set_board.find(b5.m_board) == s_set_board.end()) {
+										s_set_board.insert(b5.m_board);
+										lst.push_back(Moves());
+										lst.back().push_back(mv1);
+										lst.back().push_back(mv2);
+										lst.back().push_back(mv3);
+										lst.back().push_back(mv4);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+void HGBoard::w_genMovesList(MovesList& lst, int d1, int d2) const
+{
+	HGBoard b2(m_white, m_black);
+	b2.b_genMovesList(lst, d1, d2);
+}
+void HGBoard::b_move(const Move& mv) {
+	int dst = mv.m_src - mv.m_d;
+	if (dst < 0) dst = HG_GOAL_IX;
+	m_black[(int)mv.m_src] -= 1;
+	int rx = hg_revIX(dst);
+	if (mv.m_hit) {	//  hit
+		m_white[rx] = 0;
+		m_white[HG_START_IX] += 1;
+		//mv.m_hit = true;
+	}
+	m_black[dst] += 1;
+}
+void HGBoard::w_move(const Move& mv) {
+	int dst = mv.m_src - mv.m_d;
+	if (dst < 0) dst = HG_GOAL_IX;
+	m_white[(int)mv.m_src] -= 1;
+	int rx = hg_revIX(dst);
+	if (mv.m_hit) {	//  hit
+		m_black[rx] = 0;
+		m_black[HG_START_IX] += 1;
+		//mv.m_hit = true;
+	}
+	m_white[dst] += 1;
+}
+void HGBoard::b_move(const vector<Move>& mvs) {
+	for (const auto& mv : mvs) b_move(mv);
+}
+void HGBoard::w_move(const vector<Move>& mvs) {
+	for (const auto& mv : mvs) w_move(mv);
+}
+void HGBoard::b_unMove(const Move& mv) {
+	int dst = mv.m_src - mv.m_d;
+	if (dst < 0) dst = HG_GOAL_IX;
+	m_black[dst] -= 1;
+	int rx = hg_revIX(dst);
+	if (mv.m_hit) {	//  hit
+		m_white[rx] = 1;
+		m_white[HG_START_IX] -= 1;
+		//mv.m_hit = true;
+	}
+	m_black[(int)mv.m_src] += 1;
+}
+void HGBoard::w_unMove(const Move& mv) {
+	int dst = mv.m_src - mv.m_d;
+	if (dst < 0) dst = HG_GOAL_IX;
+	m_white[dst] -= 1;
+	int rx = hg_revIX(dst);
+	if (mv.m_hit) {	//  hit
+		m_black[rx] = 1;
+		m_black[HG_START_IX] -= 1;
+		//mv.m_hit = true;
+	}
+	m_white[(int)mv.m_src] += 1;
+}
+//	ランダムプレイアウトによる得点期待値計算、黒番固定
+//	リターン値がプラスならば黒有利
+double HGBoard::b_expScoreRPO(int N_GAME) const
+{
+	int sum = 0;
+	for (int i = 0; i < N_GAME; ++i) {
+		HGBoard b2(*this);
+		int resultSGB;
+		for(bool bt = true;; bt = !bt) {
+			MovesList lst;
+			int d1 = g_mt() % 3 + 1;
+			int d2 = g_mt() % 3 + 1;
+			if( bt ) {
+				b2.b_genMovesList(lst, d1, d2);
+				if( !lst.empty() )
+					b2.b_move(lst[g_mt() % lst.size()]);
+			} else {
+				b2.w_genMovesList(lst, d1, d2);
+				if( !lst.empty() )
+					b2.w_move(lst[g_mt() % lst.size()]);
+			}
+			if( (resultSGB = b2.resultSGB()) != 0 ) break;
+		}
+		sum += resultSGB;
+	}
+	return (double)sum / N_GAME;
+}
+//	ランダムプレイアウトによる得点期待値計算
+//	リターン値がプラスならば白有利
+double HGBoard::w_expScoreRPO(int N_GAME) const
+{
+	HGBoard b2(m_white, m_black);
+	return b2.b_expScoreRPO(N_GAME);
+}
+void HGBoard::setInput(vector<double>& input) const
+{
+	input.resize(HG_NN_INSIZE);
+	int ix = 0;
+	input[ix++] = m_black[HG_GOAL_IX] / 8.0;
+	input[ix++] = -m_white[HG_GOAL_IX] / 8.0;
+	for (int i = 1; i <= HG_N_POINT; ++i) {
+		input[ix++] = m_black[i] == 0 ? 0 : 1;
+		input[ix++] = m_black[i] <= 1 ? 0 : 1;
+		input[ix++] = m_black[i] <= 2 ? 0 : 1;
+		input[ix++] = m_black[i] <= 3 ? 0 : m_black[i] / 8.0;
+		input[ix++] = m_white[i] == 0 ? 0 : -1;
+		input[ix++] = m_white[i] <= 1 ? 0 : -1;
+		input[ix++] = m_white[i] <= 2 ? 0 : -1;
+		input[ix++] = m_white[i] <= 3 ? 0 : -m_black[i] / 8.0;
+	}
+	input[ix++] = m_black[HG_START_IX] / 2.0;
+	input[ix++] = -m_white[HG_START_IX] / 2.0;
+}
+void HGBoard::setInputNmlz(std::vector<double>& input) const				//	平均０、分散１に変換
+{
+	input.resize(HG_NN_INSIZE);
+	double sum = 0, sum2 = 0, d;
+	int ix = 0;
+	sum += d = input[ix++] = m_black[HG_GOAL_IX] / 8.0;
+	sum2 += d * d;
+	sum += d = input[ix++] = -m_white[HG_GOAL_IX] / 8.0;
+	sum2 += d * d;
+	for (int i = 1; i <= HG_N_POINT; ++i) {
+		sum += d = input[ix++] = m_black[i] == 0 ? 0 : 1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_black[i] <= 1 ? 0 : 1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_black[i] <= 2 ? 0 : 1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_black[i] <= 3 ? 0 : m_black[i] / 8.0;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_white[i] == 0 ? 0 : -1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_white[i] <= 1 ? 0 : -1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_white[i] <= 2 ? 0 : -1;
+		sum2 += d * d;
+		sum += d = input[ix++] = m_white[i] <= 3 ? 0 : -m_black[i] / 8.0;
+		sum2 += d * d;
+	}
+	sum += d = input[ix++] = m_black[HG_START_IX] / 2.0;
+	sum2 += d * d;
+	sum += d = input[ix++] = -m_white[HG_START_IX] / 2.0;
+	sum2 += d * d;
+	double ave = sum / ix;
+	double std2 = sum2 / ix - ave * ave;
+	for(auto& x: input) x = (x - ave) / sqrt(std2 + 1e-6);
+}
---
+++ HGNN/HGNN/HGBoard.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#define	 HG_N_POINT		 12
+#define	 HG_ARY_SIZE		(HG_N_POINT+2)	 //  +2 for スタート、ゴール
+#define	 HG_BOARD_SIZE	  HG_ARY_SIZE*2
+#define	 HG_START_IX		(HG_ARY_SIZE-1)
+#define	 HG_GOAL_IX		 0
+#define	 HG_INNER		   3			   //  インナー [1, 3]
+#define	 HG_MAX_DICE		3			   //  ダイスの目：[1, 3]
+
+#define		HG_NN_INSIZE		(HG_N_POINT*2*4+2+2)
+#define		HG_NN_HIDSIZE		40
+
+static inline int hg_revIX(int x) { return HG_START_IX - x; }
+
+typedef const char cchar;
+
+struct Move {
+public:
+	Move(char src = 0, char d = 0, bool hit = false)
+		: m_src(src)
+		, m_d(d)
+		, m_hit(hit)
+	{
+	}
+public:
+	std::string text() const;
+public:
+	char	m_src;	  //  移動元
+	char	m_d;		//  移動距離
+	bool	m_hit;	  //  ヒット？
+};
+
+typedef std::vector<Move> Moves;
+typedef std::vector<Moves> MovesList;
+
+class HGBoard {
+public:
+	HGBoard();
+	HGBoard(const HGBoard&);
+	HGBoard(cchar* black, cchar* white);
+public:
+	bool	operator==(const HGBoard&) const;
+	bool	operator!=(const HGBoard& x) const { return !operator==(x); }
+	std::string board() const { return m_board; }
+	//std::string key() const { return m_board; }
+	std::string ktext() const;
+	std::string text() const;
+	int	b_pips() const;
+	int	w_pips() const;
+	int	result() const;		//	{-1, 0, +1} を返す
+	int	resultSGB() const;		//	ギャモン・バックギャモン勝負を判定、{-3, -2, -1, 0, +1, +2, +3} を返す
+	void	b_genMoves(Moves&, int) const;
+	void	b_genMovesListSeq(MovesList&, int, int) const;  //  d1 != d2, d1, d2 の順に使用
+	void	b_genMovesList(MovesList&, int, int) const;
+	void	w_genMovesList(MovesList&, int, int) const;
+	double	b_expScoreRPO(int N_GAME = 100) const;			//	ランダムプレイアウトによる得点期待値計算、リターン値がプラスならば黒有利
+	double	w_expScoreRPO(int N_GAME = 100) const;			//	ランダムプレイアウトによる得点期待値計算、リターン値がプラスならば白有利
+	void	setInput(std::vector<double>&) const;
+	void	setInputNmlz(std::vector<double>&) const;				//	平均０、分散１に変換
+public:
+	void	init();
+	void	clear();
+	void	swapBW();
+	void	updateNumBW();				//	白黒石数再計算
+	void	set(const std::string&);
+	void	b_setAt(int ix, int n);
+	void	w_setAt(int ix, int n);
+	void	b_move(const Move&);
+	void	w_move(const Move&);
+	void	b_move(const std::vector<Move>&);
+	void	w_move(const std::vector<Move>&);
+	void	b_unMove(const Move&);
+	void	w_unMove(const Move&);
+//private:
+public:
+	char* m_black;	   //  黒石、m_board[0] を指す
+	char* m_white;	   //  白石、m_board[HG_ARY_SIZE] を指す
+	int	 m_nBlack;
+	int	 m_nWhite;
+	std::string  m_board;		//  白黒石データ、size = HG_ARY_SIZE*2
+};
